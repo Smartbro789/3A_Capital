5f4c5c2aee436c36826d628683aa84a0f857690c
Admin <oapk2002@gmail.com>
2020-09-27 15:03:10 +0300

    lab2 page

--- js/lab2methods.js
+++ js/lab2methods.js
@@ -6,36 +6,15 @@ function mission_lab2(){
         var text = document.getElementById("t2");
         text.innerHTML = "";
         var pic1 = document.createElement("IMG");
-        var pic2 = document.createElement("IMG");
-        var pic3 = document.createElement("IMG");
-        var pic4 = document.createElement("IMG");
-        var pic5 = document.createElement("IMG");
-        var pic6 = document.createElement("IMG");
-        var pic7 = document.createElement("IMG");
-        pic1.src = "images/description/Screenshot_1.jpg";
-        pic2.src = "images/description/Screenshot_2.jpg";
-        pic3.src = "images/description/Screenshot_3.jpg";
-        pic4.src = "images/description/Screenshot_4.jpg";
-        pic5.src = "images/description/Screenshot_5.jpg";
-        pic6.src = "images/description/Screenshot_6.jpg";
-        pic7.src = "images/description/Screenshot_7.jpg";
+        pic1.src = "images/lab2/lists.jpg";
         text.appendChild(pic1);
-        text.appendChild(pic2);
-        text.appendChild(pic3);
-        text.appendChild(pic4);
-        text.appendChild(pic5);
-        text.appendChild(pic6);
-        text.appendChild(pic7);
     }
     function identificators(){
-        var text = document.getElementById("t1");
+        var text = document.getElementById("t2");
         text.innerHTML = "";
         var pic1 = document.createElement("IMG");
-        var pic2 = document.createElement("IMG");
-        pic1.src = "images/structure/Screenshot_1.jpg";
-        pic2.src = "images/structure/Screenshot_2.jpg";
+        pic1.src = "images/lab2/identificator.jpg";
         text.appendChild(pic1);
-        text.appendChild(pic2);
     }
     function paddings(){
         var text = document.getElementById("t1");
@@ -52,18 +31,24 @@ function mission_lab2(){
         text.appendChild(pic1);
     }
     function tagClassSelectors(){
-        var text = document.getElementById("t1");
+        var text = document.getElementById("t2");
         text.innerHTML = "";
         var pic1 = document.createElement("IMG");
-        pic1.src = "images/picture.jpg";
+        pic1.src = "images/lab2/tagclassselector.jpg";
         text.appendChild(pic1);
     }
     function otherSelectors(){
-        var text = document.getElementById("t1");
+        var text = document.getElementById("t2");
         text.innerHTML = "";
         var pic1 = document.createElement("IMG");
-        pic1.src = "images/mission.jpg";
+        var pic2 = document.createElement("IMG");
+        var pic3 = document.createElement("IMG");
+        pic1.src = "images/lab2/otherSelectors1.jpg";
+        pic2.src = "images/lab2/otherSelectors2.jpg";
+        pic3.src = "images/lab2/otherSelectors3.jpg";
         text.appendChild(pic1);
+        text.appendChild(pic2);
+        text.appendChild(pic3);
     }
     function selectorsGrouping(){
         var text = document.getElementById("t1");
