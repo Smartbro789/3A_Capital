975dc2c451689cb2a19523190b084ad18d5a2928
Admin <oapk2002@gmail.com>
2020-10-05 13:07:00 +0300

    lab3 napolnenie

--- js/lab3methods.js
+++ js/lab3methods.js
@@ -2,3 +2,33 @@ function mission_lab3(){
     var text = document.getElementById("t3");
     text.innerHTML = "придбати практичні навички роботи  верстки сторінок засобами CSS, верстки на основі плаваючих елементів, з’ясувати переваги та недоліки типів макетів веб-сторінок";
 }
+function frontMaket(){
+    var text = document.getElementById("t3");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    pic1.src = "images/lab3/maket.jpg";
+    text.appendChild(pic1);
+}
+function tableMarkup(){
+    var text = document.getElementById("t3");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    pic1.src = "images/lab3/table.jpg";
+    var pic2 = document.createElement("IMG");
+    pic2.src = "images/lab3/table2.jpg";
+    text.appendChild(pic1);
+    text.appendChild(pic2);
+}
+function floatMarkup(){
+    var text = document.getElementById("t3");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    pic1.src = "images/lab3/page.jpg";
+    var pic2 = document.createElement("IMG");
+    pic2.src = "images/lab3/pageCSS.jpg";
+    var pic3 = document.createElement("IMG");
+    pic3.src = "images/lab3/page2.jpg";
+    text.appendChild(pic1);
+    text.appendChild(pic2);
+    text.appendChild(pic3);
+}
