d7e1426a94fd95e1119739e1c8ce41a3ff718e10
Admin <oapk2002@gmail.com>
2020-10-26 11:20:06 +0200

    lab4Panasiuk

---
+++ js/lab4methods.js
@@ -0,0 +1,25 @@
+function mission_lab4(){
+        document.getElementById("t4").innerHTML = "придбати практичні навички роботи маніпулювання інформаційним вмістом Web-документа засобами мови JavasSript, використання об’єктів,масивів, функцій, подій, обробників подій у сценаріях на мові JavasSript";
+
+    }
+    function scheme(){
+
+    }
+    function hanlerExample(){
+
+    }
+    function entity(){
+
+    }
+    function insertion(){
+
+    }
+    function task_4(){
+
+    }
+    function task_6(){
+
+    }
+    function conclution(){
+
+    }
