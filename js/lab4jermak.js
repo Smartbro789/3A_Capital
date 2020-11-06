2e7f6a56161fecd9101e71f55a2f651ed4b13ec3
Admin <oapk2002@gmail.com>
2020-11-06 14:56:33 +0200

    gamno

---
+++ js/lab4jermak.js
@@ -0,0 +1,55 @@
+var arrpar = [];
+var arrnepar = [];
+function division(arr){
+	for(i=0; i<arr.length; i++){
+		if(i%2 == 0){
+			arrpar.push(arr[i]);
+		}else{
+			arrnepar.push(arr[i]);
+		}
+	}
+}
+function selectionSort(arr){
+    var n = arr.length;
+    for(var i=0; i < n-1; i++)
+    { var min=i;
+        for(var j = i + 1; j<n; j++){
+            if (arr[j] > arr[min]) 
+            min = j;
+        }
+        var t = arr[min];
+        arr[min] = arr[i];
+        arr[i] = t;
+    }
+    return arr;
+}
+function task3(){
+	var len = document.getElementById('length').value;
+	array = arrayRandomizer(len);
+	division(array);
+	minPar=getMinOfArray(arrpar);
+	maxPar=getMaxOfArray(arrpar);
+	minNePar=getMinOfArray(arrnepar);
+	maxNePar=getMaxOfArray(arrnepar);
+	var message1 = "Массив парних: мінімальне значення:" + minPar + " максимальне значення:" + maxPar;
+	var message2 = "Массив непарних: мінімальне значення:" + minNePar + " максимальне значення:" + maxNePar;
+	var message3 = "Початковий масив: " + array;
+	var message4 = "Відсортований масив: " + selectionSort(array);
+	document.getElementById("output1").innerHTML = message1;
+	document.getElementById("output2").innerHTML = message2;
+	document.getElementById("output3").innerHTML = message3;
+	document.getElementById("output4").innerHTML = message4;
+}
+function arrayRandomizer(len){
+	var arr = [];
+	for ( var i = 0; i < len; i++ ) {
+    	arr.push( Math.round( Math.random()*100));
+	}
+	return arr
+}
+function getMaxOfArray(numArray) {
+  return Math.max.apply(null, numArray);
+}
+function getMinOfArray(numArray) {
+  return Math.min.apply(null, numArray);
+}
--- js/lab4methods.js
+++ js/lab4methods.js
@@ -1,25 +1,63 @@
 function mission_lab4(){
-        document.getElementById("t4").innerHTML = "придбати практичні навички роботи маніпулювання інформаційним вмістом Web-документа засобами мови JavasSript, використання об’єктів,масивів, функцій, подій, обробників подій у сценаріях на мові JavasSript";
+    var text = document.getElementById("t4");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    pic1.src = "images/lab4/l4.jpg";
+    text.appendChild(pic1);
+}
+function scheme(){
+    var text = document.getElementById("t4");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    pic1.src = "images/lab4/url.jpg";
+    text.appendChild(pic1);
+}
+function handlerExample(){
+    var text = document.getElementById("t4");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    pic1.src = "images/lab4/handler.jpg";
+    text.appendChild(pic1);
+}
+function insertion(){
+    var text = document.getElementById("t4");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    pic1.src = "images/lab4/script.jpg";
+    text.appendChild(pic1);
+}
+function task_4(){
+    var text = document.getElementById("t4");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    var pic2 = document.createElement("IMG");
+    var pic3 = document.createElement("IMG");
+    var pic4 = document.createElement("IMG");
+    pic1.src = "images/lab4/task4Panasiuk.jpg";
+    pic2.src = "images/lab4/result4Panasiuk.jpg";
+    pic3.src = "images/lab4/taks4gleks.jpg";
+    pic4.src = "images/lab4/result4Panasiuk.jpg";
+    text.appendChild(pic1);
+    text.appendChild(pic2);
+    text.appendChild(pic3);
+    text.appendChild(pic4);
+}
+function task_6(){
+    var text = document.getElementById("t4");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    var pic2 = document.createElement("IMG");
+    var pic3 = document.createElement("IMG");
+    var pic4 = document.createElement("IMG");
+    pic1.src = "images/lab4/calcCode.jpg";
+    pic2.src = "images/lab4/calc.jpg";
+    pic3.src = "images/lab4/sliderCode.jpg";
+    pic4.src = "images/lab4/slider.jpg";
+    text.appendChild(pic1);
+    text.appendChild(pic2);
+    text.appendChild(pic3);
+    text.appendChild(pic4);
+}
+function conclution(){
 
-    }
-    function scheme(){
-
-    }
-    function hanlerExample(){
-
-    }
-    function entity(){
-
-    }
-    function insertion(){
-
-    }
-    function task_4(){
-
-    }
-    function task_6(){
-
-    }
-    function conclution(){
-
-    }
+}
