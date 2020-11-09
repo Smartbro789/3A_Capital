61a102fb3cefd0358eb965e833fdf00fe8c6ba01
Admin <oapk2002@gmail.com>
2020-11-09 12:53:12 +0200

    lab 5

---
+++ js/lab5methods.js
@@ -0,0 +1,74 @@
+function mission_lab5(){
+    var text = document.getElementById("t5");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    pic1.src = "images/lab5/mission.jpg";
+    text.appendChild(pic1);
+}
+function dynamicElements(){
+    var text = document.getElementById("t5");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    var pic2 = document.createElement("IMG");
+    var pic3 = document.createElement("IMG");
+    pic1.src = "images/lab5/task3_1.jpg";
+    pic2.src = "images/lab5/task3_2.jpg";
+    pic3.src = "images/lab5/task3_3.jpg";
+    text.appendChild(pic1);
+    text.appendChild(pic2);
+    text.appendChild(pic3);
+}
+function photogallery(){
+    var text = document.getElementById("t5");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    var pic2 = document.createElement("IMG");
+    pic1.src = "images/lab5/photogallery.jpg";
+    pic2.src = "images/lab5/photogallery2.jpg";
+    text.appendChild(pic1);
+    text.appendChild(pic2);
+}
+function sliderPhoto(){
+    var text = document.getElementById("t5");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    var pic2 = document.createElement("IMG");
+    pic1.src = "images/lab5/carousel.jpg";
+    pic2.src = "images/lab5/carousel2.jpg";
+    text.appendChild(pic1);
+    text.appendChild(pic2);
+}
+function sliderVideo(){
+    var text = document.getElementById("t5");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    var pic2 = document.createElement("IMG");
+    pic1.src = "images/lab5/sliderVideo.jpg";
+    pic2.src = "images/lab5/sliderVideo2.jpg";
+    text.appendChild(pic1);
+    text.appendChild(pic2);
+}
+function socialButtons(){
+    var text = document.getElementById("t5");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    var pic2 = document.createElement("IMG");
+    pic1.src = "images/lab5/socials.jpg";
+    pic2.src = "images/lab5/socials2.jpg";
+    text.appendChild(pic1);
+    text.appendChild(pic2);
+}
+function infoM(){
+    var text = document.getElementById("t5");
+    text.innerHTML = "";
+    var pic1 = document.createElement("IMG");
+    var pic2 = document.createElement("IMG");
+    pic1.src = "images/lab5/infoM.jpg";
+    pic2.src = "images/lab5/infoM2.jpg";
+    text.appendChild(pic1);
+    text.appendChild(pic2);
+}
+function conclusion_l5(){
+var text = document.getElementById("t5");
+    text.innerHTML = "Oleg: Під час виконання лабораторної роботи, я навчився з використанням jQuery додавати кнопки соціальних мереж та інформери до web-сторінки.<br>Rolik: Під час виконання лабораторної роботи, я освоїв навички роботи з плагіном jQuery, зокрема UniteGallery. Також я вдосконалив навички роботи з медіаконтентом у html.<br>Panasiuk: Навчився працювати з JQuery, створювати динамічні єлементи та використовувати їх";
+}
